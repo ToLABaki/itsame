2f0a98b1dc61df3d3daa4ecda322fec7c8fb2741
Dimitris Zervas <dzervas@dzervas.gr>
2017-10-02 20:34:08 +0300

    Initial commit

---
+++ core/user.py
@@ -0,0 +1,6 @@
+class User:
+    def register(self, username):
+        raise NotImplementedError("Registration is not implemented")
+
+    def authenticate(self, username):
+        raise NotImplementedError("Authentication is not implemented")
