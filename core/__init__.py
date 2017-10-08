78643a640f1fbd43c1b02a96a29aca2ce3c274d7
Dimitris Zervas <dzervas@dzervas.gr>
2017-10-09 01:05:19 +0300

    [core] Added decorator for APIs to call their main functions

--- core/__init__.py
+++ core/__init__.py
@@ -1,2 +1,11 @@
+_api_mains = []
+
+def api_main(*states):
+    def decorator(fn):
+        _api_mains.append(fn)
+
+        return fn
+    return decorator
+
 from .user import *
 from .exceptions import *
