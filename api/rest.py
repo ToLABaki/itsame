a37751a598a3dd3cd0166b5cbab04561f4c7ed47
Constantine K <looselyrigorous@gmail.com>
2017-10-17 00:42:35 +0300

    [api] add /register route to api prototype

--- api/rest.py
+++ api/rest.py
@@ -7,4 +7,8 @@ app = Flask(__name__)
 def login():
     return jsonify(core.User(request.form["username"], request.form["password"]))
 
+@app.route("/register", methods=["POST"])
+def register():
+    return jsonify(core.User(request.form["username"], request.form["password"], register=True))
+
 core._api_mains.append(app.run)
